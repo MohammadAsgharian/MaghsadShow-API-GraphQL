d4d8a932012fe86a1897b32f7af369fe5bdf266d
MohammadAsgharian <m.asgharian@gmail.com>
2022-03-12 07:54:13 +0330

    master

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,46 @@
 const graphql = require("graphql");
-const { GraphQLDateTime } = require("graphql-iso-date");
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const _ = require("lodash");
+
+const users = [
+  {
+    Id: "1",
+    UserName: "Mass2007",
+    FirstName: "Mohammad",
+    LastName: "Asgharian",
+  },
+
+  {
+    Id: "2",
+    UserName: "Marzieh.Vahid",
+    FirstName: "Marzieh",
+    LastName: "Vahid",
+  },
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     Id: { type: GraphQLString },
+    UserName: { type: GraphQLString },
     FirstName: { type: GraphQLString },
-    LastName: { type: GraphQLSrting },
-    Tel: { type: GraphQLSrting },
-    ActivateSms: { type: GraphQLInt },
-    AvatarPhotoName: { type: GraphQLString },
-    SinginDate: { type: GraphQLDateTime },
+    LastName: { type: GraphQLString },
   },
 });
-const resolvers = {
-  /* your other resolvers */
-  DateTime: GraphQLDateTime,
-};
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { Id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { Id: args.Id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
--- server.js
+++ server.js
@@ -1,14 +1,89 @@
 const express = require("express");
-const expressGraphQL = require("express-graphql").graphqlHTTP;
-
 const app = express();
+const users = [
+  {
+    Id: "1",
+    UserName: "Mass2007",
+    FirstName: "Mohammad",
+    LastName: "Asgharian",
+  },
+
+  {
+    Id: "2",
+    UserName: "Marzieh.Vahid",
+    FirstName: "Marzieh",
+    LastName: "Vahid",
+  },
+];
+
+const { graphqlHTTP } = require("express-graphql");
+const graphiql = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = graphiql;
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    Id: { type: GraphQLString },
+    UserName: { type: GraphQLString },
+    FirstName: { type: GraphQLString },
+    LastName: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { Id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return users;
+      },
+    },
+  },
+});
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        Id: { type: GraphQLString },
+        FirstName: { type: GraphQLString },
+        LastName: { type: GraphQLString },
+        UserName: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        users.push({
+          Id: args.Id,
+          FirstName: args.FirstName,
+          LastName: args.LastName,
+          UserName: args.UserName,
+        });
+        return args;
+      },
+    },
+  },
+});
+
+// Construct a schema, using GraphQL schema language
+const schema = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
+
 app.use(
   "/graphql",
-  expressGraphQL({
+  graphqlHTTP({
+    schema: schema,
     graphiql: true,
   })
 );
-
-app.listen(5000, () => {
-  console.log("listening");
-});
+app.listen(5000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
